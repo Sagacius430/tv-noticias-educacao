f926dfc6489335449ca429a2efb32c08d9f87c0d
Lincoln Hawff <33042055+Sagacius430@users.noreply.github.com>
2025-06-08 21:01:27 -0300

    final

---
+++ gerar_json.py
@@ -0,0 +1,12 @@
+[
+  {
+    "titulo": "Alunos participam de feira de ciências",
+    "resumo": "Evento reúne projetos inovadores de escolas estaduais",
+    "link": "https://www.educacao.sp.gov.br/noticias/alunos-participam-de-feira"
+  },
+  {
+    "titulo": "Nova plataforma de ensino é lançada",
+    "resumo": "Ferramenta permitirá aulas mais interativas",
+    "link": "https://www.educacao.sp.gov.br/noticias/nova-plataforma-de-ensino"
+  }
+]
